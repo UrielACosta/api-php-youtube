503d4141d0b8559a7b7b24b7bf0f755a09477ee1
uriel.alves1@hotmail.com <uriel.alves1@hotmail.com>
2019-12-23 10:52:32 -0300

    [Fix] Checking if the limit parameter is an integer

--- API/index.php
+++ API/index.php
@@ -7,19 +7,21 @@ require_once('controller/SearchController.php');
 if (isset($_GET)){
 
   $term = '';
-  $limit = '';
+  $limit = 0;
 
   if (isset($_GET['term'])){
 
     $term = $_GET['term'];
 
     if (isset($_GET['limit'])){  
-      if (is_int($_GET['limit'])){
-           $limit = $_GET['limit'];   
+
+      if (ctype_digit($_GET['limit'])){
+        $limit = $_GET['limit'];
+     
       }else{
-        $limit = 10;
+        $limit = 10;  
       }
-  
+
     }else{
       $limit = 10;
     }
